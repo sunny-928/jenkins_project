3f111702bee2e46a1182ba0f30ee2d83e43f212e
sunny-928 <645276764@qq.com>
2019-11-25 14:32:59 +0800

    first commit

---
+++ scripts/test_login.py
@@ -0,0 +1,20 @@
+import allure
+import pytest
+class TestLogin:
+
+    # @allure.step(title="输入")
+    def test_login1(self):
+        # self.driver = driver
+        # allure.attach("关键字", "pass")
+        assert 1
+        # allure.attach("截图：", self.driver.get_screenshot_as_png(), allure.attach_type.PNG)
+
+    def test_login2(self):
+        assert 1
+
+    # @pytest.allure.severity(pytest.allure.severity_level.TRIVIAL)
+    def test_login3(self):
+        assert 0
+
+    def test_login4(self):
+        assert 1
